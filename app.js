9c6320596d629111f387910ea1daa2f78beae010
Tommy9607 <244300712@qq.com>
2018-04-24 18:38:54 +0800

    update index, lost&found

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ var indexRoutes = require("./routes/index");
 
 app.use("/", indexRoutes);
 
+
+
+app.get("/lost_and_found", function(req, res){
+    res.render("lost_and_found");
+});
+
+app.get("/lost_and_found_map", function(req, res){
+    res.render("lost_and_found_map");
+});
+
 app.listen(3000, function () {
    console.log("The iManager Server Started at 3000");
 });
